43ccf41f79b4e95bc69e1146c65f623ff8794560
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2025-11-22 01:22:03 +0530

    update userSchema

--- model/userSchema.js
+++ model/userSchema.js
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+import validator from "validator";
 
 const userSchema = new mongoose.Schema(
   {
@@ -6,18 +8,22 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
       trim: true,
+      maxlength: 50
     },
     email: {
       type: String,
       required: true,
       unique: true,
       trim: true,
+      maxlength: 100,
+      validator: [validator.isEmail, "Invalid email ID"]
     },
     password: {
       type: String,
       required: true,
       trim: true,
       select: false,
+      minlength: 8
     },
     role: {
       type: String,
@@ -27,6 +33,14 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+userSchema.index({email:1}, {unique:true})
+
+userSchema.pre("save", (next) => {
+    const SALT = bcrypt.genSaltSync(10)
+    this.password = bcrypt.hashSync(this.password, SALT)
+    next()
+})
 
 const user = mongoose.model('User', userSchema);
 
