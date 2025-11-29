069d43d5fd66f82c05e2cc336520aa62cc9964d9
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2025-11-29 11:29:45 +0530

    update createTask and getAllTask

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -1,27 +1,75 @@
-import Task from '../models/taskSchema';
+import Task from '../models/taskSchema.js';
+import User from '../models/userSchema.js';
 
 export const createTask = async (req, res) => {
   try {
-    const { title, description, priority, assignedTo, createdBy } = req.body;
+    const { title, description, assignedTo } = req.body;
+
+    const creator = await User.findOne({ name: req.userName });
+
+    if (!creator) {
+      return res
+        .status(404)
+        .json({ success: false, message: 'Creator not found' });
+    }
+
+    const assigner = await User.findOne({ name: assignedTo });
+
+    if (!assigner) {
+      return res
+        .status(404)
+        .json({ success: false, message: 'Assigner not found' });
+    }
+
     const task = {
       title: title,
       description: description,
-      priority: priority,
-      assignedTo: assignedTo,
-      createdBy: createdBy,
+      assignedTo: assigner._id,
+      createdBy: creator._id,
     };
+
     const newTask = await Task.create(task);
-    res.status(201).json({ success: true, data: newTask });
+
+    const populateTask = await Task.findById(newTask._id)
+      .populate('assignedTo', 'name')
+      .populate('createdBy', 'name');
+
+    const cleaned = {
+      _id: populateTask._id,
+      title: populateTask.title,
+      description: populateTask.description,
+      status: populateTask.status,
+      priority: populateTask.priority,
+      assignedTo: populateTask.assignedTo?.name,
+      createdBy: populateTask.createdBy?.name,
+    };
+
+    res.status(201).json({ success: true, data: cleaned });
   } catch (error) {
     console.log('Error while creating task: ', error);
-    res.status(500).json({ success: false, message: 'Fail: new task created' });
+    res
+      .status(500)
+      .json({ success: false, message: 'Fail to create new task' });
   }
 };
 
 export const getAllTasks = async (req, res) => {
   try {
-    const allTasks = await Task.find();
-    res.status(201).json({ success: true, data: allTasks });
+    const allTasks = await Task.find()
+      .populate('assignedTo', 'name')
+      .populate('createdBy', 'name');
+    const formatTask = allTasks.map((task) => ({
+      _id: task._id,
+      title: task.title,
+      description: task.description,
+      status: task.status,
+      priority: task.priority,
+      assignedTo: task.assignedTo?.name,
+      createdBy: task.createdBy?.name,
+      createdAt: task.createdAt,
+      updatedAt: task.updatedAt,
+    }));
+    res.status(201).json({ success: true, data: formatTask });
   } catch (error) {
     console.log('Fail to get all tasks: ', error);
     res.status(500).json({ success: false, message: 'Fail to fetch all task' });
