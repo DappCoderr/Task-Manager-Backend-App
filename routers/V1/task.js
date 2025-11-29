3165c41fcfe516d59757086ceba9ee37d311293e
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2025-11-29 11:30:17 +0530

    update task router

--- routers/V1/task.js
+++ routers/V1/task.js
@@ -5,14 +5,15 @@ import {
   getAllTasks,
   getTaskById,
   updateTaskById,
-} from '../../controllers/task.controller';
+} from '../../controllers/task.controller.js';
+import { verifyToken } from '../../middlewares/auth.mw.js';
 
 const route = express.Router();
 
 route.get('/', getAllTasks);
 route.get('/user/:id', getTaskById);
 
-route.post('/newTask', createTask);
+route.post('/newTask', verifyToken, createTask);
 route.put('/update/:id', updateTaskById);
 
 route.delete('/delete/:id', deleteTaskById);
